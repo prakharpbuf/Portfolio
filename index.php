31d98dfc64754b95aec199458e0d306abced22f0
prakharpbuf <prakhar.paw08@gmail.com>
2020-07-16 18:00:12 -0400

    Day 1 colorful: Desktop view

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ function test_input($data){
         <span id="NameContainer"><span id="myName"><strong>Prakhar</strong></span></span>
         <a href="mailto:prakhar.paw08@gmail.com" class="active">Email</a>
         <a href="#Projects">Projects</a>
-        <a href="#Skills">Skills</a>
+        <a href="#Skills" style="margin-right:-2%">Skills</a>
         <a href="#Certificates">Certificates</a>
         <a href="#Profile">Home</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
@@ -44,21 +44,26 @@ function test_input($data){
         </a>
     </div>
     <section id="Profile">
-        <br>
         <h1>
-            <strong>Here's my profile...</strong>
+            <strong style="line-height:53px; font-weight:500">Hi, I'm Prakhar Paw...</strong>
         </h1>
-        <br>
-        Hi, I'm Prakhar Paw... <br>
-        The content in this website is yet to be filled in but you are welcome to check out the layout!! <br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut tellus ac ligula mattis rutrum vel et massa. Class aptent
-        taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis fermentum dui at quam varius, nec
-        accumsan nisi faucibus
-        <br> <br> 
-        <button id="knowMore">Download resume</button> &nbsp;(PDF)
-        <br> <br> <br>
+        <span class="para">
+An extremely enthusiastic student, currently enrolled at SUNY at Buffalo class of '21. Having a very strong base in Computer Science, I am looking for a job in IT/Cybersecurity while I work towards achieving more and more relevant skills and certifications.
+A ridiculously quick learner who is always looking to learn and master new skills. 
+
+        </span> <br>
+        <button id="knowMore">Download resume</button> &nbsp; <strong>(PDF)</strong>
+    </section>
+    <section id="photo">
+        <div id='photoYellow'></div>
+        <div id='photoBlue'></div>
+        <img src="photo.jpg" alt="Prakhar's photo">
     </section>
     </header>
+    <section id="Certificates">
+        <h1 class="sectionHeading"><strong>My Certificates</strong></h1>
+        <button id='certificatesBtn'>See All</button>
+    </section>
     <script>
         function expand(sec){
             var btn=document.getElementById(sec.id);
@@ -78,10 +83,10 @@ function test_input($data){
         }
     </script>
     <section id="Skills"> 
-    <h1 class="sectionHeading headingSkills"><strong>Skills</strong></h1>
-    <div class="Skill firstSkill"><i class="icon fas fa-code fa-2x"></i> <span class="Description"><strong>Modern Networking </strong><br>Designing and implementing functional networks. Configuring, managing, and <span id='dots_1'>....</span> <span id='more_1'>maintaining essential network devices. Using devices such as switches and routers to segment network traffic and create resilient networks. Identifying the benifits and drawbacks of existing network configurations. Implementing network security, standards and protocols. Troubleshooting network problems. Supporting the creation of virtualized networks.</span></span><br><button onclick="expand(this)" id='button_1' class='btnSkill'>Read More &darr;</button></div>
-    <div class="Skill"><i class="icon fab fa-python fa-2x"></i><span class="Description"> <strong>Cybersecurity </strong><br>Hands-on network security. Risk management. Threat intelligence. Data<span id='dots_2'>....</span> <span id='more_2'>and host security. Identity management. Cryptography and public key infrastructure (PKI). Common tools like nmap, BurpSuite, etc. Cybersecurity best practices. System hardening.</span></span><br><button onclick="expand(this)" id='button_2' class='btnSkill'>Read More &darr;</button></div>
-    <div class="Skill"><i class="icon fas fa-code fa-2x"></i><span class="Description"><strong>Python</strong> <br>Language syntax, Data structures in python. Using python to <span id='dots_3'>....</span> <span id='more_3'> access web data. Retrieving, Processing and Visualizing data in Python. Using databases (SQLite3) with python. pillow, tesseract, and openCV. Web crawling and web scraping. RegExes. Troubleshooting, interacting with and automating tasks with python </span></span> <br><button onclick="expand(this)" id='button_3' class='btnSkill'>Read More &darr;</button></div>
+    <h1 class="sectionHeading headingSkills"><strong>My Skills</strong></h1>
+    <div class="Skill firstSkill"><i class="icon fas fa-network-wired fa-2x"></i> <span class="Description"><strong>Modern Networking </strong><br>Designing and implementing functional networks. Confi<span id='dots_1'>....</span> <span id='more_1'>guring, managing, and maintaining essential network devices. Using devices such as switches and routers to segment network traffic and create resilient networks. Identifying the benifits and drawbacks of existing network configurations. Implementing network security, standards and protocols. Troubleshooting network problems. Supporting the creation of virtualized networks.</span></span><br><button onclick="expand(this)" id='button_1' class='btnSkill'>Read More &darr;</button></div>
+    <div class="Skill"><i class="icon fas fa-lock fa-2x"></i><span class="Description"> <strong>Cybersecurity </strong><br>Hands-on network security. Risk management. Threat<span id='dots_2'>....</span> <span id='more_2'> intelligence. Data and host security. Identity management. Cryptography and public key infrastructure (PKI). Common tools like nmap, BurpSuite, etc. Cybersecurity best practices. System hardening.</span></span><br><button onclick="expand(this)" id='button_2' class='btnSkill'>Read More &darr;</button></div>
+    <div class="Skill"><i class="icon fab fa-python fa-2x"></i><span class="Description"><strong>Python</strong> <br>Language syntax, Data structures in python. Using python to<span id='dots_3'>....</span> <span id='more_3'> access web data. Retrieving, Processing and Visualizing data in Python. Using databases (SQLite3) with python. pillow, tesseract, and openCV. Web crawling and web scraping. RegExes. Troubleshooting, interacting with and automating tasks with python </span></span> <br><button onclick="expand(this)" id='button_3' class='btnSkill'>Read More &darr;</button></div>
     <div class="Skill"><i class="icon fab fa-python fa-2x"></i><span class="Description"><strong>C </strong><br> Language syntax, data types, preprocessor operations, functi<span id='dots_4'>....</span><span id='more_4'>ons, dynamic memory allocation and deallocation, pointers, file handling and multithreading and mutex using pthread library</span></span><br><button onclick="expand(this)" id='button_4' class='btnSkill'>Read More &darr;</button></div>
     <div id='moreSkillsContainer'>
     <div class="Skill"><i class="icon fas fa-code fa-2x"></i><span class="Description"><strong>C++</strong> <br>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut tellus ac ligula<span id='dots_5'>....</span> <span id='more_5'>mattis rutrum vel et massa. Class aptent
@@ -90,7 +95,7 @@ function test_input($data){
     taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</span></span> <br><button onclick="expand(this)" id='button_6' class='btnSkill'>Read More &darr;</button></div>
     <div class="Skill"><i class="icon fas fa-code fa-2x"></i><span class="Description"><strong>Git and GitHub</strong> <br>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut tellus ac ligula<span id='dots_7'>....</span> <span id='more_7'>mattis rutrum vel et massa. Class aptent
     taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</span></span> <br><button onclick="expand(this)" id='button_7' class='btnSkill'>Read More &darr;</button></div>
-    <div class="Skill"><i class="icon fas fa-code fa-2x"></i><span class="Description"><strong>MS Access and other MS Office software</strong> <br>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut tellus ac ligula<span id='dots_8'>....</span> <span id='more_8'>mattis rutrum vel et massa. Class aptent
+    <div class="Skill"><i class="icon fas fa-code fa-2x"></i><span class="Description"><strong>MS Office</strong> <br>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut tellus ac ligula<span id='dots_8'>....</span> <span id='more_8'>mattis rutrum vel et massa. Class aptent
     taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</span></span> <br><button onclick="expand(this)" id='button_8' class='btnSkill'>Read More &darr;</button></div>
     <div class="Skill"><i class="icon fas fa-code fa-2x"></i><span class="Description"><strong>Java</strong> <br>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut tellus ac ligula<span id='dots_9'>....</span> <span id='more_9'>mattis rutrum vel et massa. Class aptent
     taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</span></span> <br><button onclick="expand(this)" id='button_9' class='btnSkill'>Read More &darr;</button></div>
@@ -151,12 +156,12 @@ function test_input($data){
             <h3 class="subsectionHeading">SDOOP and DBMS</h3>
             <span class="Description">
                 Outlined a graphics editor application with create, edit, save, select, zoom, group, and clipboard functionality using OOP for logic and StarUML for design and made this application interactive using SQL and C++ to manage data.
-                <br> <br>
+                <br> <br> <br> 
                 <a href=""><strong>READ MORE&nbsp;</strong> <i class="fas fa-arrow-right"></i></a>
                 <br>
             </span>
         </div>
-        <button class="viewWork" onclick="window.location.href='#'">&uarr; &nbsp; TOP &nbsp; &uarr;</button>
+        <button class="viewWork" onclick="window.location.href=''">View work &rarr;</button>
     </section>
     <footer id="Contact">
         <h1 class="sectionHeading contact"><strong>Get in touch...</strong></h1>
@@ -187,17 +192,17 @@ function test_input($data){
         <div id="social">
             <br>
             <span id="LinkedIn">
-                <a href=""><i class="fab fa-linkedin-in fa-2x"></i></a>
+                <a href=""><i class="fab fa-linkedin-in"></i></a>
             </span>
             <span id="FB">
-               <a href="https://www.facebook.com/prakhar.paw.37"><i class="fab fa-facebook-f fa-2x"></i></a>
+               <a href="https://www.facebook.com/prakhar.paw.37"><i class="fab fa-facebook-f"></i></a>
             </span>
             <span id="Instagram">
-               <a href="https://www.instagram.com/prakhar.paw/"><i class="fab fa-instagram-square fa-2x"></i></a>
+               <a href="https://www.instagram.com/prakhar.paw/"><i class="fab fa-instagram-square"></i></a>
             </span>
         </div>
         <div id="copyright">
-            Prakhar Paw - I'm the best
+            Prakhar Paw (I'm the best)
         </div>
     </footer>
 </body>
